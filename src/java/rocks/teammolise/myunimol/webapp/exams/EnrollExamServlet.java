576855958b1041178fc1cd90494ec5ee442e7cb7
intersimone999 <intersimone999@hotmail.it>
2014-12-18 14:19:18 +0100

    Integrazione della servlet EnrollExamServlet

---
+++ src/java/rocks/teammolise/myunimol/webapp/exams/EnrollExamServlet.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rocks.teammolise.myunimol.webapp.exams;
+
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import rocks.teammolise.myunimol.api.APIConsumer;
+import rocks.teammolise.myunimol.webapp.UserInfo;
+
+/**
+ *
+ * @author Silvio
+ */
+
+
+@WebServlet(name = "EnrollExamServlet", urlPatterns = {"/EnrollExamServlet"})
+public class EnrollExamServlet extends HttpServlet {
+	private static final long serialVersionUID = -1944631703912934540L;
+
+	/**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+       
+        PrintWriter out = response.getWriter();
+        
+        try{
+         
+            if(request.getSession().getAttribute("userInfo") == null){
+                
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+                return;
+                
+            }
+            
+            UserInfo userInfo = (UserInfo)request.getSession().getAttribute("userInfo");
+            
+            Map<String, Object> params = new HashMap<String, Object>();
+            
+            String username = userInfo.getUsername();
+            String password = userInfo.getPassword();
+            String examId = request.getParameter("id");
+            String notes = request.getParameter("notes");
+            
+            if (examId == null || notes == null) {
+            	response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Bad request");
+            	return;
+            }
+            	
+            
+            params.put("id", examId);
+            params.put("notes", notes);
+            
+            JSONObject enrollExamJSON = new APIConsumer().consume("enrollExam", username, password, params);
+           
+            out.print(enrollExamJSON);
+            
+        } catch (UnirestException ex) {        
+            
+             response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Internal server error");
+             
+        }finally{
+            out.close();
+        }
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
