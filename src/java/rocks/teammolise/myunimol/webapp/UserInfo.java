a346ab52f4d4e0b0fd7bab07687c88ab54dff10b
Matteo Merola <mattmezza@gmail.com>
2014-12-23 01:04:17 +0100

    Updated UserInfo to match the api

--- src/java/rocks/teammolise/myunimol/webapp/UserInfo.java
+++ src/java/rocks/teammolise/myunimol/webapp/UserInfo.java
@@ -2,108 +2,110 @@ package rocks.teammolise.myunimol.webapp;
 
 /**
  * Il bean del'utente con le informazioni da inserire nella sessione
+ * 
  * @author Federica Mauriello
  */
 public class UserInfo {
-    private String name;
-    private String surname;
-    private String studentId;
-    private String studentClass;
-    private String username;
-    private String password;
-    private String taxes;
-    private String careerPlan;
-    private String availableExams;
-    private String enrolledExams;
-    
-    private String course;
-    private String department;
-    private String coursePath;
-    private int courseLength;
-    private String registrationDate;
+	private String name;
+	private String surname;
+	private String studentId;
+	private String studentClass;
+	private String username;
+	private String password;
+	private String taxes;
+	private String careerPlan;
+	private int availableExams;
+	private int enrolledExams;
 
-    public String getTaxes() {
-        return taxes;
-    }
+	private String course;
+	private String department;
+	private String coursePath;
+	private int courseLength;
+	private String registrationDate;
 
-    public void setTaxes(String taxes) {
-        this.taxes = taxes;
-    }
+	public String getTaxes() {
+		return taxes;
+	}
 
-    public String getCareerPlan() {
-        return careerPlan;
-    }
+	public void setTaxes(String taxes) {
+		this.taxes = taxes;
+	}
 
-    public void setCareerPlan(String careerPlan) {
-        this.careerPlan = careerPlan;
-    }
+	public String getCareerPlan() {
+		return careerPlan;
+	}
 
-    public String getAvailableExams() {
-        return availableExams;
-    }
+	public void setCareerPlan(String careerPlan) {
+		this.careerPlan = careerPlan;
+	}
 
-    public void setAvailableExams(String availableExams) {
-        this.availableExams = availableExams;
-    }
+	public int getAvailableExams() {
+		return availableExams;
+	}
 
-    public String getEnrolledExams() {
-        return enrolledExams;
-    }
+	public void setAvailableExams(int availableExams) {
+		this.availableExams = availableExams;
+	}
 
-    public void setEnrolledExams(String enrolledExams) {
-        this.enrolledExams = enrolledExams;
-    }
-    public UserInfo(){
-        
-    }
+	public int getEnrolledExams() {
+		return enrolledExams;
+	}
 
-    public String getUsername() {
-        return username;
-    }
+	public void setEnrolledExams(int enrolledExams) {
+		this.enrolledExams = enrolledExams;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public UserInfo() {
 
-    public String getPassword() {
-        return password;
-    }
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public String getSurname() {
-        return surname;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getStudentId() {
-        return studentId;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setStudentId(String userId) {
-        this.studentId = userId;
-    }
+	public String getSurname() {
+		return surname;
+	}
 
-    public String getStudentClass() {
-        return studentClass;
-    }
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
 
-    public void setStudentClass(String studentClass) {
-        this.studentClass = studentClass;
-    }
+	public String getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(String userId) {
+		this.studentId = userId;
+	}
+
+	public String getStudentClass() {
+		return studentClass;
+	}
+
+	public void setStudentClass(String studentClass) {
+		this.studentClass = studentClass;
+	}
 
 	public String getCourse() {
 		return course;
@@ -144,7 +146,7 @@ public class UserInfo {
 	public void setRegistrationDate(String registrationDate) {
 		this.registrationDate = registrationDate;
 	}
-	
+
 	public int getTotalCFU() {
 		return this.courseLength * 60;
 	}
--- src/java/rocks/teammolise/myunimol/webapp/login/LoginServlet.java
+++ src/java/rocks/teammolise/myunimol/webapp/login/LoginServlet.java
@@ -23,106 +23,123 @@ import rocks.teammolise.myunimol.webapp.UserInfo;
  * 
  * @author Federica Mauriello
  */
-@WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
+@WebServlet(name = "LoginServlet", urlPatterns = { "/LoginServlet" })
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 2857028958624319384L;
 
 	/**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        //il tipo di risultato della servlet
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
+	 * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+	 * methods.
+	 *
+	 * @param request
+	 *            servlet request
+	 * @param response
+	 *            servlet response
+	 * @throws ServletException
+	 *             if a servlet-specific error occurs
+	 * @throws IOException
+	 *             if an I/O error occurs
+	 */
+	protected void processRequest(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// il tipo di risultato della servlet
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
 
-        try {            
-            //sono per ricavare il contenuto degli input field
-            String username = (String) request.getParameter("username");
-            String password = (String) request.getParameter("password");
-            
-            JSONObject loginJSON = new APIConsumer().consume("testCredentials", username, password);
-            
-            if (loginJSON.getString("result").equals("failure")) {
-                out.print(loginJSON.toString());
-            } else {
-                //inserisco i dati presi dal JSON nel bean dell'utente
-            	UserInfo userInfo = new UserInfo();
-                userInfo.setName(loginJSON.getString("name"));
-                userInfo.setSurname(loginJSON.getString("surname"));
-                userInfo.setStudentClass(loginJSON.getString("studentClass"));
-                userInfo.setStudentId(loginJSON.getString("studentID"));
-                userInfo.setTaxes(loginJSON.getString("taxes"));
-                userInfo.setCareerPlan(loginJSON.getString("careerPlan"));
-                userInfo.setAvailableExams(loginJSON.getString("availableExams"));
-                userInfo.setEnrolledExams(loginJSON.getString("enrolledExams"));
-                userInfo.setUsername(username);
-                userInfo.setPassword(password);
-                
-                userInfo.setCourse(loginJSON.getString("course"));
-                userInfo.setDepartment(loginJSON.getString("department"));
-                userInfo.setCoursePath(loginJSON.getString("coursePath"));
-                userInfo.setCourseLength(loginJSON.getInt("courseLength"));
-                userInfo.setRegistrationDate(loginJSON.getString("registrationDate"));
-                
-                HttpSession session = request.getSession(true);
-                session.setAttribute("userInfo", userInfo);
-                
-                out.print("{\"result\": \"correct\"}");
-            }
-        } catch (UnirestException e) {
-        	response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Internal Server Error");
-        } catch (JSONException e) {
-        	response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Internal Server Error");
-        } finally {
-            out.close();
-        }
+		try {
+			// sono per ricavare il contenuto degli input field
+			String username = (String) request.getParameter("username");
+			String password = (String) request.getParameter("password");
 
-    }
+			JSONObject loginJSON = new APIConsumer().consume("testCredentials",
+					username, password);
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+			if (loginJSON.getString("result").equals("failure")) {
+				out.print(loginJSON.toString());
+			} else {
+				// inserisco i dati presi dal JSON nel bean dell'utente
+				UserInfo userInfo = new UserInfo();
+				userInfo.setName(loginJSON.getString("name"));
+				userInfo.setSurname(loginJSON.getString("surname"));
+				userInfo.setStudentClass(loginJSON.getString("studentClass"));
+				userInfo.setStudentId(loginJSON.getString("studentID"));
+				userInfo.setTaxes(loginJSON.getString("taxes"));
+				userInfo.setCareerPlan(loginJSON.getString("careerPlan"));
+				userInfo.setAvailableExams(loginJSON.getInt("availableExams"));
+				userInfo.setEnrolledExams(loginJSON.getInt("enrolledExams"));
+				userInfo.setUsername(username);
+				userInfo.setPassword(password);
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+				userInfo.setCourse(loginJSON.getString("course"));
+				userInfo.setDepartment(loginJSON.getString("department"));
+				userInfo.setCoursePath(loginJSON.getString("coursePath"));
+				userInfo.setCourseLength(loginJSON.getInt("courseLength"));
+				userInfo.setRegistrationDate(loginJSON
+						.getString("registrationDate"));
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+				HttpSession session = request.getSession(true);
+				session.setAttribute("userInfo", userInfo);
+
+				out.print("{\"result\": \"correct\"}");
+			}
+		} catch (UnirestException e) {
+			 response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+			 "Internal Server Error");
+		} catch (JSONException e) {
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+					"Internal Server Error");
+		} finally {
+			out.close();
+		}
+
+	}
+
+	// <editor-fold defaultstate="collapsed"
+	// desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+	/**
+	 * Handles the HTTP <code>GET</code> method.
+	 *
+	 * @param request
+	 *            servlet request
+	 * @param response
+	 *            servlet response
+	 * @throws ServletException
+	 *             if a servlet-specific error occurs
+	 * @throws IOException
+	 *             if an I/O error occurs
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	/**
+	 * Handles the HTTP <code>POST</code> method.
+	 *
+	 * @param request
+	 *            servlet request
+	 * @param response
+	 *            servlet response
+	 * @throws ServletException
+	 *             if a servlet-specific error occurs
+	 * @throws IOException
+	 *             if an I/O error occurs
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	/**
+	 * Returns a short description of the servlet.
+	 *
+	 * @return a String containing servlet description
+	 */
+	@Override
+	public String getServletInfo() {
+		return "Short description";
+	}// </editor-fold>
 
 }
