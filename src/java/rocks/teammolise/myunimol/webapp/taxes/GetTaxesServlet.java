b4364c405ee221502b08b4eea7e053e7b7de78d2
cbranca <c.branca3@gmail.com>
2014-12-26 20:44:58 +0100

    Added Taxes Servlet

---
+++ src/java/rocks/teammolise/myunimol/webapp/taxes/GetTaxesServlet.java
@@ -0,0 +1,92 @@
+package rocks.teammolise.myunimol.webapp.taxes;
+
+import com.mashape.unirest.http.exceptions.UnirestException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.JSONObject;
+
+import rocks.teammolise.myunimol.api.APIConsumer;
+import rocks.teammolise.myunimol.webapp.UserInfo;
+
+/**
+ *
+ * @author Carlo Branca
+ */
+@WebServlet(name = "GetTaxesServlet", urlPatterns = {"/GetTaxesServlet"})
+
+public class GetTaxesServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        response.setContentType("application/json;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        try {
+            if (request.getSession().getAttribute("userInfo") == null) {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+                return;
+            }
+
+            UserInfo userInfo = (UserInfo) request.getSession().getAttribute("userInfo");
+
+            String username = userInfo.getUsername();
+            String password = userInfo.getPassword();
+
+            JSONObject taxesJSON = new APIConsumer().consume("getTaxes", username, password);
+            
+            out.print(taxesJSON);
+            
+        } catch (UnirestException ex) {
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Internal server error");
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
