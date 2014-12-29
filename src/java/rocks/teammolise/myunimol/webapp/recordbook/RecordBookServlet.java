54dfcf627f6729692ee4dd3e0316b2eb5a691180
cbranca <c.branca3@gmail.com>
2014-12-29 14:06:28 +0100

    Minor fixes

--- src/java/rocks/teammolise/myunimol/webapp/recordbook/RecordBookServlet.java
+++ src/java/rocks/teammolise/myunimol/webapp/recordbook/RecordBookServlet.java
@@ -26,7 +26,7 @@ public class RecordBookServlet extends HttpServlet {
 	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
-        response.setContentType("text/html;charset=UTF-8");
+        response.setContentType("application/json;charset=UTF-8");
         PrintWriter out = response.getWriter();
                 
         try { 
